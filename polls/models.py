c7c072a6735326bd3519aa37de299707f3b37acc
cloudflips32 <devadamstandish@gmail.com>
2024-12-03 00:40:56 -0500

    models cont.'d,py shell exercises completed, admin page is up

--- polls/models.py
+++ polls/models.py
@@ -1,11 +1,24 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 class Question(models.Model):
+    
+    def __str__(self):
+        return self.question_text
+
     question_text = models.CharField(max_length=48)
     pub_date = models.DateTimeField("date published")
 
+    def was_publised_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
 class Choice(models.Model):
-    # ForeignKey takes in a 
+    # ForeignKey signifies that id will be read for each question 
+    def __str__(self):
+        return self.choice_text
+    
     question = models.ForeignKey(Question,on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=48)
     votes = models.IntegerField(default=0)
