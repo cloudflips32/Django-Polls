d08cb5cce5fda4fa4301074f2d39e96643abe438
cloudflips32 <devadamstandish@gmail.com>
2024-12-02 22:47:30 -0500

    data models, models.py

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+    question_text = models.CharField(max_length=48)
+    pub_date = models.DateTimeField("date published")
+
+class Choice(models.Model):
+    # ForeignKey takes in a 
+    question = models.ForeignKey(Question,on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=48)
+    votes = models.IntegerField(default=0)
--- polls/views.py
+++ polls/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-  return HttpResponse("Hello world, This will eventually be where polls are displayed.")
+    return HttpResponse("Hello world, This will eventually be where polls are displayed.")
